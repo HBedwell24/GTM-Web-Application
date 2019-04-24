2e3316f319dcac208ef9e15d67bcaddd27430f5f
HBedwell24 <32681500+HBedwell24@users.noreply.github.com>
2019-04-24 11:16:21 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,3 @@
 <?php
-$con = mysqli_connect('localhost','root','', 'gtm-web-database');
-
+$con = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
  ?>
